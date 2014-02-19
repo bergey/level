2e3653bf11f076c70fe62d4fa654ea3000e15834
Daniel Bergey <bergey@alum.mit.edu>
2014-02-19 20:57:13 +0000

    Update for new STL using Float

--- src/Graphics/Formats/STL/Linear.hs
+++ src/Graphics/Formats/STL/Linear.hs
@@ -6,11 +6,17 @@ import Graphics.Formats.STL
 import Linear
 import Linear.Affine
 
-triangle :: V3 Double -> V3 (Point V3 Double) -> Triangle
-triangle n pts = Triangle (asTriple n) . asTriple $ (asTriple . view lensP <$> pts)
+triangle :: Real a => V3 a -> V3 (Point V3 a) -> Triangle
+triangle n pts = Triangle (asVector n) . asTriple $ (asVector . view lensP <$> pts)
 
 asTriple :: V3 a -> (a,a,a)
 asTriple (V3 x y z) = (x,y,z)
 
+asVector :: Real a => V3 a -> (Float,Float,Float)
+asVector (V3 x y z) = (r2f x,r2f y,r2f z)
+
 asV3 :: (a,a,a) -> V3 a
 asV3 (x,y,z) = V3 x y z
+
+r2f :: (Real r, Fractional f) => r -> f
+r2f = realToFrac
