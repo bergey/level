851cbd2cfd98304d5c5cbf6db8bdb2e511291e6d
Daniel Bergey <bergey@alum.mit.edu>
2014-11-08 18:05:44 +0000

    WIP: funnel model

--- src/Graphics/Level/Shapes.hs
+++ src/Graphics/Level/Shapes.hs
@@ -14,20 +14,25 @@ sphere :: Implicit
 sphere = Implicit (\p -> {-# SCC "sphereFun" #-} qdA p origin - 1) (\_ -> {-# SCC "sphereEnv" #-} -1 ... 1)
 
 box :: Implicit
-box = Implicit (\p -> {-# SCC "boxFun" #-} maximum [p^._x - 1, p^._y - 1, p^._z - 1, -1 - p^._x, -1 - p^._y, -1 - p^._z])
-               (\v -> {-# SCC "boxEnv" #-} (pure (-1.1) `dot` v) ... (pure 1.1 `dot` v))
-       -- TODO require by contract that v is a unit vector
-  -- where
-  --   manhattan (V3 x y z) = x+y+z
+box = Implicit
+      (\p -> maximum [p^._x - 1, p^._y - 1, p^._z - 1, -1 - p^._x, -1 - p^._y, -1 - p^._z])
+      (boxExtents $ V3 1.1 1.1 1.1)
+
+boxExtents :: Vec -> Vec -> Interval Double
+boxExtents (V3 xl yl zl) (V3 x y z) = -d ... d where
+  d = maximum [xl // x, yl // y, zl // z]
+  a // b | b == 0 = 0
+  a // b = a / b
 
 -- | @genCyl rMax f@ is a generalized cylinder defined by a radius
 -- function @f@.  More precisely, for z = [0,1], it is the union of
 -- circles of radius @f z@ perpendicular to the z-axis and centered at
 -- [0,0,z].  @rMax@ is the maximum value of @f@ in the domain, and is
 -- needed to construct the bounding envelope.
--- genCyl :: (Double -> Double) -> Implicit
--- genCyl rMax f = Implicit im env where
---   im p = sqrt (dot v v) (dot v unitZ) - f (p ^. _z) where
---     v = p .-. origin
---     unitZ = V3 0 0 1
---   env = pure . dot (V3 rMax rMax 1)
+genCyl :: Double -> (Double -> Double) -> Implicit
+genCyl rMax f = Implicit im (boxExtents $ V3 rMax rMax 1) where
+  im :: Pt -> Double
+  im p = maximum [radial, z - 1, -z] where
+    radial = sqrt (quadrance v - z ^ (2 :: Int)) - f z
+    v = p .-. origin
+    z  = p ^. _z
--- src/Graphics/Level/Util.hs
+++ src/Graphics/Level/Util.hs
@@ -11,6 +11,9 @@ import Control.Lens
 translate :: Vec -> Implicit -> Implicit
 translate v (Implicit f env) = Implicit (f . (.-^ v)) (\u -> pure (v `dot` normalize u) + env u)
 
+translateZ :: Double -> Implicit -> Implicit
+translateZ z = translate $ V3 0 0 z
+
 intersect :: Implicit -> Implicit -> Implicit
 intersect (Implicit f env1) (Implicit g env2) = Implicit fg env where
   fg p = max (f p) (g p)
@@ -19,12 +22,17 @@ intersect (Implicit f env1) (Implicit g env2) = Implicit fg env where
 union :: Implicit -> Implicit -> Implicit
 union (Implicit f env1) (Implicit g env2) = Implicit fg env where
   fg p = min (f p) (g p)
-  env p = max <$> env1 p <*> env2 p
+  env p = max <$> env1 p <*> env2 p -- TODO is this right?  Not if `inf (env1 p)` is negative?
 
 scale :: Double -> Implicit -> Implicit
 scale s (Implicit f env) = Implicit f' env' where
   f' p = f $ p & lensP . mapped //~ s
   env' p = (*s) <$> env p
+
+scaleZ :: Double -> Implicit -> Implicit
+scaleZ s (Implicit f env) = Implicit f' env' where
+  f' = f . (_z //~ s)
+  env' = env . (_z //~ s)
 
 -- difference can have a tighter bound than union
 -- negate has an infinite bound, so we don't do that to Implicits
